abdafe0ae315212990ef91ffd795d4574edec742
Yoanna Popova <yoanna.dp@gmail.com>
2025-02-28 19:32:44 +0200

    Add changing name functionality.

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,31 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handlerEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+
+            <button onClick={handlerEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 } 
