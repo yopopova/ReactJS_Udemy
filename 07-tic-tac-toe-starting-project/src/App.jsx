a0fe40c4c23fccf1c0d8e89e69d12a6acd2d1901
Yoanna Popova <yoanna.dp@gmail.com>
2025-02-28 18:04:16 +0200

    Add Player component into App.

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,17 @@
+import Player from "./components/Player";
+
 function App() {
   return (
-    <p>Comming soon...</p>
+    <main>
+        <div id="game-container">
+            <ol id="players">
+                <Player name="Player 1" symbol="X" />
+                <Player name="Player 2" symbol="O" />
+            </ol>
+
+            GAME BOARD
+        </div>
+    </main>
   );
 }
 
