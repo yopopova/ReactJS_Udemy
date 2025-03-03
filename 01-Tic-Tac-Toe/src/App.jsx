8737d525d100078cb8be08137b1f33500012afb5
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-03 13:09:23 +0200

    Set up App and GameBoard components state.

--- 01-Tic-Tac-Toe/src/App.jsx
+++ 01-Tic-Tac-Toe/src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, 
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,9 +33,9 @@ function App() {
                 <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
                 <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
             </ol>
-            <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+            <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
         </div>
-        LOG
+        <Log />
     </main>
   );
 }
