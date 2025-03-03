dd136a110ca8276a6bae185c1909335e3be547c6
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-03 11:53:03 +0200

    Lifting state and set up active player.

--- 01-Tic-Tac-Toe/src/components/GameBoard.jsx
+++ 01-Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -4,17 +4,19 @@ const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
-]
+];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = 'X';
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+
+        onSelectSquare();
     }
 
     return (
