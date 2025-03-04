2f941ee3632051b1ce98bfe60af80f096b46e52c
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-04 12:47:47 +0200

    Set up Results component.

--- 02-Investment-Calculator/src/components/Results.jsx
+++ 02-Investment-Calculator/src/components/Results.jsx
@@ -1,7 +1,35 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
 export default function Results({ input }) {
-    console.log(input);
+    const resultsData = calculateInvestmentResults(input);
+    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment; 
     
     return (
-        <p>Results...</p>
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {resultsData.map(yearData => {
+                    const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+                    const totalAmmountInvested = yearData.valueEndOfYear - totalInterest;
+
+                    return <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(totalAmmountInvested)}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
     );
 }
