9cd97d45651ad07a40a8c8d7eadc39aec12741de
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-04 10:19:04 +0200

    Add conditional statement for edit to the Player component.

--- 01-Tic-Tac-Toe/src/components/Player.jsx
+++ 01-Tic-Tac-Toe/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handlerEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
