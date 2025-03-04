2d5bad92304dcd2701030b46e1edcb3ac9718ba1
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-04 12:48:17 +0200

    Validate duration preperty.

--- 02-Investment-Calculator/src/App.jsx
+++ 02-Investment-Calculator/src/App.jsx
@@ -12,11 +12,13 @@ function App() {
         duration: 10
     });
 
+    const inputIsValid = userInput.duration >= 1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput(prevUserInput => {
             return {
                 ...prevUserInput,
-                [inputIdentifier]: newValue
+                [inputIdentifier]: Number(newValue)
             }
         });
     }
@@ -25,7 +27,8 @@ function App() {
         <>
             <Header />
             <UserInput userInput={userInput} onChange={handleChange} />
-            <Results input={userInput} />
+            {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+            {inputIsValid && <Results input={userInput} />}
         </>
     )
 }
