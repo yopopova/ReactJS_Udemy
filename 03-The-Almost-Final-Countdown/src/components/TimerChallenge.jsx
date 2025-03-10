b6329d97772727b2c29020cab6f9c242b5b62726
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-10 09:31:33 +0200

    Create TimeChallenge component.

---
+++ 03-The-Almost-Final-Countdown/src/components/TimerChallenge.jsx
@@ -0,0 +1,18 @@
+export default function TimerChallenge({ title, targetTime }) {
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} seconds{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button>
+                    Start Challenge
+                </button>
+            </p>
+            <p className="active">
+                Time is running...
+            </p>
+        </section>
+    );
+}
