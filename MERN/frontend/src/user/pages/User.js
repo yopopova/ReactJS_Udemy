f21023e9f3d26828fc3d4541b98a37284508c563
Yoanna Popova <yoanna.dp@gmail.com>
2025-03-17 10:20:00 +0200

    Create main User component.

--- MERN/frontend/src/user/pages/User.js
+++ MERN/frontend/src/user/pages/User.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>Users works!</h2>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Yoh Popova',
+            image: 'https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg',
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS} />
 }
 
 export default Users;
